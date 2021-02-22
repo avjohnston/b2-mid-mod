ac2849b8d44c8c7d1fa5c32dacc156ac88590f5f
Andrew Johnston <av.johnst@gmail.com>
2021-02-22 09:46:41 -0700

    Finish user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mech1 = Mechanic.create!(name: "Andrew", years_experience: 4)
+mech2 = Mechanic.create!(name: "Khoa", years_experience: 3)
+mech3 = Mechanic.create!(name: "Doug", years_experience: 5)
+
+ride1 = Ride.create!(name: "The Punisher", thrill_rating: 9, open: true)
+ride2 = Ride.create!(name: "The Hurler", thrill_rating: 7, open: false)
+ride3 = Ride.create!(name: "Ninja", thrill_rating: 8, open: true)
+ride4 = Ride.create!(name: "Back Breaker", thrill_rating: 8, open: true)
+ride5 = Ride.create!(name: "Spine Tingler", thrill_rating: 7, open: false)
+ride6 = Ride.create!(name: "Widow Maker", thrill_rating: 10, open: true)
+
+RideMechanic.create!(ride:ride1 , mechanic:mech1)
+RideMechanic.create!(ride:ride3 , mechanic:mech1)
+RideMechanic.create!(ride:ride2 , mechanic:mech2)
+RideMechanic.create!(ride:ride4 , mechanic:mech2)
+RideMechanic.create!(ride:ride6 , mechanic:mech3)
+RideMechanic.create!(ride:ride5 , mechanic:mech3)
+RideMechanic.create!(ride:ride2 , mechanic:mech3)
---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "mechanics index page", type: :feature do
+  before :each do
+    @mech1 = Mechanic.create!(name: "Andrew", years_experience: 4)
+    @mech2 = Mechanic.create!(name: "Khoa", years_experience: 3)
+    @mech3 = Mechanic.create!(name: "Doug", years_experience: 5)
+    @ride1 = Ride.create!(name: "The Punisher", thrill_rating: 9, open: true)
+    @ride2 = Ride.create!(name: "The Hurler", thrill_rating: 7, open: false)
+    @ride3 = Ride.create!(name: "Ninja", thrill_rating: 8, open: true)
+    @ride4 = Ride.create!(name: "Back Breaker", thrill_rating: 8, open: true)
+    @ride5 = Ride.create!(name: "Spine Tingler", thrill_rating: 7, open: false)
+    @ride6 = Ride.create!(name: "Widow Maker", thrill_rating: 10, open: true)
+    RideMechanic.create!(ride:@ride1 , mechanic:@mech1)
+    RideMechanic.create!(ride:@ride3 , mechanic:@mech1)
+    RideMechanic.create!(ride:@ride2 , mechanic:@mech2)
+    RideMechanic.create!(ride:@ride4 , mechanic:@mech2)
+    RideMechanic.create!(ride:@ride6 , mechanic:@mech3)
+    RideMechanic.create!(ride:@ride5 , mechanic:@mech3)
+    RideMechanic.create!(ride:@ride2 , mechanic:@mech3)
+  end
+  it "shows mechanics and attributes along with avg experience" do
+    visit "/mechanics"
+
+    expect(page).to have_content("All Mechanics")
+    expect(page).to have_content("#{@mech1.name}")
+    expect(page).to have_content("Years Experience: #{@mech1.years_experience}")
+    expect(page).to have_content("#{@mech2.name}")
+    expect(page).to have_content("Years Experience: #{@mech2.years_experience}")
+    expect(page).to have_content("#{@mech3.name}")
+    expect(page).to have_content("Years Experience: #{@mech3.years_experience}")
+    expect(page).to have_content("Average Age of Experience For All Mechanics")
+    expect(page).to have_content(Mechanic.average_experience)
+  end
+end
